d531d20a72205f0c10a5bcfb66bb3c34c94aaf3a
LB Johnston <lb.johnston@virginaustralia.com>
2019-04-29 07:56:45 +1000

    Rework tests to use the CLIEngine instead of Linter from eslint
    - Linter was not loading the plugins

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,29 @@
 const fs = require('fs');
 const eslint = require('eslint');
-const config = require('..');
 
-const { Linter } = eslint;
+const { CLIEngine } = eslint;
+const { readFileSync } = fs;
 
-const validExample = fs.readFileSync('./examples/valid.js', 'utf-8');
-const invalidExample = fs.readFileSync('./examples/invalid.js', 'utf-8');
+const validExample = readFileSync('./examples/valid.js', 'utf-8');
+const invalidExample = readFileSync('./examples/invalid.js', 'utf-8');
 
-const linter = new Linter();
+const cli = new CLIEngine({
+  useEslintrc: false,
+  configFile: '.eslintrc',
+});
 
 describe('flags no warnings with valid js', () => {
-  const messages = linter.verify(validExample, config, {
-    filename: 'valid.js',
-  });
+  const { errorCount, warningCount } = cli.executeOnText(validExample);
 
-  it('should return empty messages', () => expect(messages).toHaveLength(0));
+  it('should return no errors or warnings', () => {
+    expect(errorCount).toEqual(0);
+    expect(warningCount).toEqual(0);
+  });
 });
 
 describe('flags warnings with invalid js', () => {
-  const messages = linter.verify(invalidExample, config, {
-    filename: 'invalid.js',
-  });
+  const { errorCount, warningCount } = cli.executeOnText(invalidExample);
 
-  it('should return non-empty messages', () => expect(messages).not.toHaveLength(0));
+  it('should return some errors or warnings', () =>
+    expect(errorCount + warningCount).not.toEqual(0));
 });
